447991ffcc602426ee00da393ccf33c1cb9a6592
Profsain <profsaincrown@gmail.com>
2023-03-29 09:47:21 +0000

    add api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    get 'random_greeting', to: 'api#random_greeting'
+  end
+  
   # Defines the root path route ("/")
   root 'root#index'
 end
